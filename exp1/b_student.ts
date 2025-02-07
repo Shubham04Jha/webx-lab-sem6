104cdd7af5b372be5545ca3e2dd0dfc98e618f62
Shubham04jha <shubhamjha70491@gmail.com>
2025-02-07 10:14:13 +0530

    done

--- exp1/b_student.ts
+++ exp1/b_student.ts
@@ -1,29 +1,54 @@
-class Student{
-    name: string; studentId:number; grade: string;
-    constructor(name:string,id:number, grade:string){
-        this.name = name; this.studentId = id; this.grade = grade;
+class LibraryAccount {
+    accountId:number;
+    booksIssued:number;
+    constructor(accid:number,bookIss:number) {
+        this.accountId = accid;
+        this.booksIssued = bookIss;
     }
-    getDetails():string{
-        return `name: ${this.name} \n id: ${this.studentId} \n grade: ${this.grade}`;
+  
+    getLibraryInfo(): void {
+      console.log(`Account ID: ${this.accountId}, Books Issued: ${this.booksIssued}`);
     }
 }
 
-class GraduateStudent extends Student{
-    thesisTopic:string;
-    constructor(name:string,id:number,grade:string,thesisTopic:string){
-        super(name,id,grade);
+class Student {
+    name: string;
+    studentId: number;
+    grade: string;
+    libraryAccount: LibraryAccount;
+
+    constructor(name: string, id: number, grade: string, accountId: number, booksIssued: number) {
+        this.name = name;
+        this.studentId = id;
+        this.grade = grade;
+        this.libraryAccount = new LibraryAccount(accountId, booksIssued);
+    }
+
+    getDetails(): string {
+        return `Name: ${this.name} \nID: ${this.studentId} \nGrade: ${this.grade} \nLibrary Account ID: ${this.libraryAccount.accountId} \nBooks Issued: ${this.libraryAccount.booksIssued}`;
+    }
+}
+
+class GraduateStudent extends Student {
+    thesisTopic: string;
+
+    constructor(name: string, id: number, grade: string, accountId: number, booksIssued: number, thesisTopic: string) {
+        super(name, id, grade, accountId, booksIssued);
         this.thesisTopic = thesisTopic;
     }
+
     getDetails(): string {
-        return `name: ${this.name} \n id: ${this.studentId} \n grade: ${this.grade} \n Thesis Topic: ${this.thesisTopic}`
+        return `${super.getDetails()} \nThesis Topic: ${this.thesisTopic}`;
     }
-    getThesis():string{
-        return `${this.thesisTopic}`
+
+    getThesis(): string {
+        return `${this.thesisTopic}`;
     }
 }
 
-const st = new Student("Shubham",19,"A")
-console.log(st.getDetails())
+// Example usage
+const st = new Student("Shubham", 19, "A", 101, 3);
+console.log(st.getDetails());
 
-const gradSt = new GraduateStudent("Kshitij", 18, "A+", "Artificial Intelligence");
+const gradSt = new GraduateStudent("Kshitij", 18, "A+", 102, 5, "Artificial Intelligence");
 console.log(gradSt.getDetails());
